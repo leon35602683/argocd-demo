54d460130d7dfa73b6f5d6140a7cdde762724981
Leon <leon35602683@gmail.com>
2026-04-14 14:50:15 +0800

    feat: really update main.py this time

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {
-        "version": os.getenv("APP_VERSION", "v9.0"),
+        "version": os.getenv("APP_VERSION", "v10.0"),
         "status": "ok",
     }
